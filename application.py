1e759b3556922e3e4f26f299cbe7b0f5ac67e5c2
cheYurii <131592033+cheYurii@users.noreply.github.com>
2023-09-18 18:01:16 +0300

    Add files via upload

---
+++ application.py
@@ -0,0 +1,6 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+app = QApplication([])
+main_win = QWidget()
+main_win.show()
+app.exec_()
