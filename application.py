f639a1ca274dc09152d536e3064899100026caa2
cheYurii <131592033+cheYurii@users.noreply.github.com>
2023-09-25 17:47:14 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,24 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from random import randint
 app = QApplication([])
 main_win = QWidget()
+main_win.setWindowTitle("Визначник переможця")
+main_win.resize(500,500)
+button = QPushButton("Згенерувати")
+text = QLabel("Натисни, щоб дізнатись переможця")
+winner = QLabel("?")
+
+line = QVBoxLayout()
+line.addWidget(text, alignment = Qt.AlignCenter)
+line.addWidget(winner, alignment = Qt.AlignCenter)
+line.addWidget(button, alignment = Qt.AlignCenter)
+main_win.setLayout(line)
+def show_winner():
+    number = randint(0,99)
+    winner.setText(str(number))
+    text.setText("Переможець:")
+button.clicked.connect(show_winner)
 main_win.show()
 app.exec_()
+ 
